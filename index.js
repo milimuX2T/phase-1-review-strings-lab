1a88c9d491f8b87dde1dfdd736224b74b67caca4
Mathews <mathews.milimu@student.moringaschool.com>
2022-08-15 11:48:36 +0300

    Lab done

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+const currentUser = 'Milimu!';
+
+const welcomeMessage = 'Welcome to Flatbook, ' + currentUser;
+
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + "MILIMU!";
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
 // Write your code in this file!
